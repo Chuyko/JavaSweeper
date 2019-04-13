2e42c1bc3db6fb7758788074d655965f0855e71f
Dima Chuyko <cdi880@gmail.com>
2019-04-13 21:59:09 +0300

    To create constructor JavaSweeper class.

--- src/JavaSweeper.java
+++ src/JavaSweeper.java
@@ -4,4 +4,11 @@ public class JavaSweeper extends JFrame {
     public static void main(String[] args) {
        new JavaSweeper().setVisible(true);
     }
+    public JavaSweeper(){
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Java Sweeper");
+        setLocationRelativeTo(null);
+        setVisible(true);
+        setResizable(false);
+    }
 }

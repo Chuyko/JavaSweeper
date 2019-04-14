6e757b6345f7503656a1307e2690bbe9085667e0
Dima Chuyko <cdi880@gmail.com>
2019-04-14 12:16:05 +0300

    Fixed panel size.

--- src/JavaSweeper.java
+++ src/JavaSweeper.java
@@ -1,14 +1,23 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class JavaSweeper extends JFrame {
+    JPanel panel;
+
     public static void main(String[] args) {
-       new JavaSweeper().setVisible(true);
+        new JavaSweeper().setVisible(true);
     }
-    public JavaSweeper(){
+
+    public JavaSweeper() {
+        panel = new JPanel();
+        setPreferredSize(new Dimension(500, 300));
+        add(panel);
+
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setTitle("Java Sweeper");
-        setLocationRelativeTo(null);
         setVisible(true);
         setResizable(false);
+        pack();
+        setLocationRelativeTo(null);
     }
 }
